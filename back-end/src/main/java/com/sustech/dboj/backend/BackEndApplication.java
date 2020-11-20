5f12bd2b550b6632f07c66fd786c5d55ac050ebe
zc-BEAR <xiongzc2018@gmail.com>
2020-11-20 20:24:22 +0800

    cache enable

--- back-end/src/main/java/com/sustech/dboj/backend/BackEndApplication.java
+++ back-end/src/main/java/com/sustech/dboj/backend/BackEndApplication.java
@@ -2,9 +2,12 @@ package com.sustech.dboj.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class BackEndApplication {
+    //mvn package -DskipTests
 //nohup java -jar back-end-0.0.1-SNAPSHOT.jar >backend.log 2>&1 &
     public static void main( String[] args ) {
         SpringApplication.run( BackEndApplication.class , args );
